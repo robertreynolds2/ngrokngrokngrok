c6d71cb8dc4c68ca7eb5660a6a76e5d3a81a45d5
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-09-22 19:28:31 +0700

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -4,5 +4,10 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp --region ap 3389 &>/dev/null &
+echo "United States (Dallas)"
+echo "Europe (Frankfurt)"
+echo "Asia/Pacific (Singapore)"
+echo "Australia (Sydney)"
+read -p "Chose Region: " CRP1 
+nohup ./ngrok tcp --region CRP1 3389 &>/dev/null &
 echo Downloading File From akuh.net
