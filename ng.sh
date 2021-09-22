a43b8a857ca7df89d7c20bf07a9f697f208726dc
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-09-22 19:07:40 +0700

    Create ng.sh

---
+++ ng.sh
@@ -0,0 +1,8 @@
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3389 &>/dev/null &
+echo Downloading File From akuh.net
